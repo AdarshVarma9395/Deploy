7c8427be872435f82b9a6bc1c9daba31d0bfb5f9
AdarshVarma9395 <adarshvarma1999@gmail.com>
2024-01-29 16:33:11 +0530

    code commit

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-4aik-k8jn+*+t8mtakd8$xal86vq-=z3m5l866ovrpfesp%ce0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
